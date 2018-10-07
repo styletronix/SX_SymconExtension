9873517f967f844353d60296cba8e7a74370d20e
styletronix <andreas.pross@styletronix.net>
2018-10-07 21:03:48 +0200

    Add files via upload

---
+++ WatchDog/module.php
@@ -0,0 +1,86 @@
+<?
+    class WatchDog extends IPSModule {
+        public function __construct($InstanceID) {
+            parent::__construct($InstanceID);
+        }
+		
+        public function Create() {
+            parent::Create();
+
+			// $this->RegisterVariableInteger("alarmmodus", "Status", "SX_Alarm.Modus");
+			// $this->EnableAction("alarmmodus");
+						
+			$this->RegisterPropertyString("devices", null);
+			$this->RegisterPropertyString("BatteryMonitoring", null);
+			$this->RegisterPropertyInteger("refreshInterval", 60);
+			
+			
+			$this->RegisterTimer("timer_refresh", 0, 'IPS_RequestAction($_IPS["TARGET"], "TimerCallback", "timer_refresh");');
+        }
+		
+        public function ApplyChanges() {
+            parent::ApplyChanges();
+
+			
+			$this->SetTimerInterval("timer_refresh", $this->ReadPropertyInteger("refreshInterval") * 1000);
+			$this->SetStatus(102);
+        }
+		
+
+		private function GetDeviceParameters(string $Liste){
+			$arrString = $this->ReadPropertyString($Liste);
+			if ($arrString){
+				$arr = json_decode($arrString, true);
+												
+				return $arr;
+			}	
+			return null;
+		}
+		private function GetDeviceParameter(string $Liste, int $DeviceID){			
+			$arr = $this->GetDeviceParameters($Liste);
+			if ($arr){
+				foreach($arr as $key1) {
+					if($key1["InstanceID"] == $DeviceID){
+						return $key1;
+					}
+				}
+			}			
+						
+			return null;
+		}
+		private function CheckDevices(){
+			
+		}
+			
+		private function TimerCallback(string $TimerID){
+			switch($TimerID) {
+				case "timer_refresh":
+					$this->CheckDevices();
+					break;
+					
+				default:
+					$this->SetTimerInterval($TimerID, 0);
+					throw new Exception("Invalid TimerCallback");
+
+    		}
+		}
+		
+		public function RequestAction($Ident, $Value) {
+			switch($Ident) {
+			case "TimerCallback":
+					$this->TimerCallback($Value);
+					break;
+				
+				default:
+					throw new Exception("Invalid Ident");
+
+    		}
+ 		}
+		
+		public function MessageSink($TimeStamp, $SenderID, $Message, $Data) {
+			if ($Message == 10603){
+				
+			}
+		}
+    }
+?>
---
+++ WatchDog/readme.md
@@ -0,0 +1,31 @@
+#  WatchDog
+---
+## Dokumentation
+
+**Inhaltsverzeichnis**
+
+1. [Funktionsumfang](#1-funktionsumfang)
+2. [Systemanforderungen](#2-systemanforderungen)
+3. [Installation](#3-installation)
+4. [Änderungen](#4-änderungen)
+---
+
+
+## 1. Funktionsumfang
+- Der WatchDog überwacht Geräte auf Aktualisierung und löst eine Warnung aus, falls Geräte innerhalb einer gewissen Zeitspanne keine aktualisierung melden.
+---
+
+
+## 2. Systemanforderungen
+- IP-Symcon ab Version 5.0
+---
+
+
+## 3. Installation
+1. Fügen Sie in der IP-Symcon Managementkonsole im Objektbaum unter "Kern Instanzen" / "Modules" die URL `https://github.com/styletronix/SX_SymconExtension`als neues Modul hinzu.
+2. Fügen Sie an einer beliebigen Stelle im Objektbaum die Instanz  `WatchDog` hinzu.
+3. Führen Sie die Konfiguration im Instanzeditor durch.
+---
+
+## 4. Änderungen
+
