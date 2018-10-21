a746880152f053063d7b4a8f151977c69d42edb8
styletronix <andreas.pross@styletronix.net>
2018-10-21 12:38:49 +0200

    Add files via upload

--- WatchDog/module.php
+++ WatchDog/module.php
@@ -14,6 +14,8 @@
 			$this->RegisterPropertyString("BatteryMonitoring", "[{\"Caption\":\"HomeMatic\",\"ModuleID\":\"{EE4A81C6-5C90-4DB7-AD2F-F6BBD521412E}\",\"Ident\":\"LOWBAT\"},{\"Caption\":\"HomeMatic\",\"ModuleID\":\"{EE4A81C6-5C90-4DB7-AD2F-F6BBD521412E}\",\"Ident\":\"LOW_BAT\"}]");
 			$this->RegisterPropertyInteger("refreshInterval", 60);
 			
+			$this->RegisterVariableString("BattMon_OK", "");
+			$this->RegisterVariableString("BattMon_Empty", "");
 			
 			$this->RegisterTimer("timer_refresh", 0, 'IPS_RequestAction($_IPS["TARGET"], "TimerCallback", "timer_refresh");');
         }
@@ -49,7 +51,29 @@
 			return null;
 		}
 		private function CheckDevices(){
+			$BattMonitorTableEmpty = "";
+			$BattMonitorTableOK = "";
 			
+			$arr = GetDeviceParameters("BatteryMonitoring");
+			if ($arr){
+				foreach ($arr as $value) {
+				$instances = IPS_GetInstanceListByModuleID($value["ModuleID"]);
+				foreach ($instances as $instance) {
+					$valID = @IPS_GetObjectIDByIdent($value["Ident"], $instance["InstanceID"]);
+					if ($valID){
+						$BattLevel = GetValue($valID);
+						if ($BattLevel){
+							$BattMonitorTableEmpty .= IPS_GetName($instance["InstanceID"]) . "<br>"
+						}else{
+							$BattMonitorTableOK .= IPS_GetName($instance["InstanceID"]) . "<br>";
+						}						
+					}
+				}
+			}
+			}
+			
+			SetValueString($this->GetIDForIdent("BattMon_OK"), $BattMonitorTableOK);
+			SetValueString($this->GetIDForIdent("BattMon_Empty"), $BattMonitorTableEmpty);
 		}
 			
 		private function TimerCallback(string $TimerID){
