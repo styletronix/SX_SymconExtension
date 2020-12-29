b12a5961a9bad7dc648100306b9f8d014d38290c
styletronix <andreas.pross@styletronix.net>
2020-12-29 11:29:10 +0100

    Add files via upload

--- Treppenhauslicht/module.php
+++ Treppenhauslicht/module.php
@@ -18,10 +18,13 @@
 			
 			$this->RegisterPropertyInteger("on_time", 240);
 			$this->RegisterPropertyInteger("off_warning_time", 30);
+			$this->RegisterPropertyBoolean("showStatus", false);
 			
 			$this->RegisterTimer("warning_timer",0,'IPS_RequestAction($_IPS["TARGET"], "TimerCallback", "warning_timer");');
 			$this->RegisterTimer("on_timer",0,'IPS_RequestAction($_IPS["TARGET"], "TimerCallback", "on_timer");');
 			$this->RegisterTimer("blink_timer",0,'IPS_RequestAction($_IPS["TARGET"], "TimerCallback", "blink_timer");');
+			
+			$this->ParameterChanged("showStatus");
         }
 		
         public function ApplyChanges() {
@@ -48,7 +51,22 @@
 			$this->SetTimerInterval("warning_timer", 0);
 			
 			$this->SetBuffer("IsOn", "true");
+			$this->SetValue("statusString", "Zeit läuft...");
 			$this->SetAllDeviceStatus(true);				
+		}
+		
+		public function ParameterChanged(string $parameter){
+			if ($parameter == "showStatus"){
+				$showStatus = $this->ReadPropertyBoolean("showStatus");
+				if ($showStatus){
+					$this->RegisterVariableString("statusString","Status");
+				}else{
+					$id = $this->GetIDForIdent("statusString");
+					if ($id){
+						$this->UnregisterVariable($id);
+					}					
+				}
+			}
 		}
 		
 		private function SetAllDeviceStatus(bool $Value){
@@ -95,7 +113,7 @@
 						
 			return null;
 		}
-		private function Blink(){
+		private function Blink(){		
 			$this->SetAllDeviceStatus(false);
 			IPS_Sleep(1000);
 			
@@ -107,6 +125,7 @@
 		
 		private function Off(){
 			$this->SetBuffer("IsOn", "false");
+			$this->SetValue("statusString", "Aus");
 			$this->SetAllDeviceStatus(false);
 		}
 		
@@ -122,6 +141,7 @@
 					$offWarningTime = $this->ReadPropertyInteger("off_warning_time");
 					if ($offWarningTime > 0){
 						$this->SetTimerInterval("warning_timer", $offWarningTime * 1000);
+						$this->SetValue("statusString", "Ausschaltwarnung");
 						$this->Blink();
 					}else{
 						$this->Off();
